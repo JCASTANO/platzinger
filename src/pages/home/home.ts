5c0d00f9be878b9a2461c6df8cf80640d80db1cf
Juan Esteban <juan.castano@ceiba.com.co>
2018-08-26 20:32:10 -0500

    5to: interfaces

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { User } from './../../interfaces/user';
 import { LoginPage } from './../login/login';
 import { ConversationPage } from './../conversation/conversation';
 import { Component } from '@angular/core';
@@ -9,8 +10,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(private navCtrl: NavController) {
+  friends: User[] = [];
 
+  constructor(private navCtrl: NavController) {
+    let usuario1: User = {
+      nick: 'Eduardo',
+      email: 'a@gmail.com',
+      friend: true,
+      uid : 1
+    };
+
+    let usuario2: User = {
+      nick: 'Juan',
+      email: 'juan@gmail.com',
+      friend: true,
+      uid : 1
+    };
+
+    this.friends = [usuario1,usuario2];
   }
 
   goToConversation() {
